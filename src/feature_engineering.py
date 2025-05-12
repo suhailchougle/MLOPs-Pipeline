51050478c8f972552eb0f36a06d6d8e0b879787a
suhailchougle <sufian.chougle1996@gmail.com>
2025-05-12 12:47:48 +0400

    Pipeline with DVC, without params

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -96,7 +96,7 @@ def main():
     try:
         # params = load_params(params_path='params.yaml')
         # max_features = params['feature_engineering']['max_features']
-        max_features = 50
+        max_features = 51
 
         train_data = load_data('./data/interim/train_processed.csv')
         test_data = load_data('./data/interim/test_processed.csv')
